9bc175ac61b6cfe45e08cc156da9d345ff6e03d2
ozlem-demir <ozlem.demi10@gmail.com>
2024-12-07 15:16:36 +0300

    third change

--- app/src/main/java/com/acm431/teamup/MainActivity.kt
+++ app/src/main/java/com/acm431/teamup/MainActivity.kt
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     TeamUpTheme {
-        Greeting("Android Ozlem")
+        Greeting("Android Ozlem!")
     }
 }
